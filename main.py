64fae4f39f499c5aad1754a9502a26ae867872d7
RaptorExTolop <jamesappletonret@gmail.com>
2024-10-08 18:02:56 +1030

    2: added the intro cl area

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
+# import section
 import random
 
+#
+new_line = "\n"
+
+
 def main():
-    intro()
-    range = get_range() # range should have three numbers, two for the range and one for the number of guesses
-    number_guessing(range)
+    name = intro()
+    range = get_range(name) # range should have three numbers, two for the range and one for the number of guesses
+    number_guessing(name, range)
 
 def intro():
-    end_line = "\n"
     intro = f"Hello there user, and welcome to the random number game!"
     simple_explanation = "The game is simple, input a range of number, eg 1 and 6, and then the no. of guesses you are allowed."
-    print()
+    print(intro, new_line, simple_explanation)
+    name = input("May I ask what you name is? ")
+    input(f"Thank you, {name}. Press any button to begin!")
+    return name
